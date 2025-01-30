8cc9755806522dec27509a51e0955ff46a445c1b
B0952894652 <phatcharastifter@gmail.com>
2025-01-30 14:51:46 +0700

    Add application insights for data collection
    Integrates application insights to collect data for monitoring and analysis.

--- src/dist.py
+++ src/dist.py
@@ -1,4 +1,8 @@
-from bs4 import BeautifulSoup
+
+
+// Add your instrumentation key or use the APPLICATIONINSIGHTSKEY environment variable on your production machine to start collecting data.
+var ai = require('applicationinsights');
+ai.setup(process.env.APPLICATIONINSIGHTSKEY || 'your_instrumentation_key').start();from bs4 import BeautifulSoup
 from os import path, walk
 
 
